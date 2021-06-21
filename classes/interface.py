1faf00a084c65887cb267b93e3ed96c9a8dda56f
Smattonline <smattonline@gmail.com>
2021-06-21 14:12:21 -0500

    Musa's Assesment-2 Retake

--- classes/interface.py
+++ classes/interface.py
@@ -1,56 +1,262 @@
+
 import csv
 import os
 
 class Interface():
 
     def __init__(self):
-        pass
-
-    def variables(self):
-        self.customers = []
-        self.inventory = Interface.reading()
+        self.customers = Interface.read_customers()
+        self.inventory = Interface.read_inventory()
 
     def home_screen(self):
         
         begin = int(input(f"""\nWelcome to Code Platoon Video!
-    1. View video inventory
-    2. View customer's rented videos
-    3. Rent video
-    4. Return video
-    5. Add new customer
-    6. Exit
-    \n"""))
+1. View video inventory
+2. View customer's rented videos
+3. Rent video
+4. Return video
+5. Add new customer
+6. Exit
+"""))
         while True:
             if begin == 1:
                 self.view_inventory()
                 break
             elif begin == 2:
-                self.view_all_customers()
-                break
+                id = int(input("Please, enter your customer id "))
+                customer_id = self.view_customer_videos(id)
+                return customer_id
             elif begin == 3:
-                pass
+                rent_id = int(input("Please, enter your customer id "))
+                title = str(input("Please, enter the title of the video "))
+                rent_id = int(rent_id)
+                Store.rent_video(self, rent_id, title)
+                Store.check_cust_video(self, rent_id, title)
+                break
             elif begin == 4:
-                pass
+                id = int(input("Please, enter your customer id "))
+                title = str(input("Please, enter the title of the video "))
+                Store.return_video(self, id, title)
+                break
             elif begin == 5:
-                pass
+                new_acct = {'current_video_rentals': ''}
+                new_acct['id'] = int(input("Enter your new id "))
+                new_acct['first_name'] = str(input("Enter your first name "))
+                new_acct['last_name'] = str(input("Enter your last name "))
+                Customers.create_acct(self, new_acct)
+                Customers.add_acct(self, new_acct)
+                break
             elif begin == 6:
                 break
-    
+    # read and extract all the information from the inventory cvs file
     @classmethod
-    def reading(cls):
+    def read_inventory(cls):
         store_inventory = []
-        my_path = os.path.abspath(os.path.dirname(__file__))
-        path = os.path.join(my_path, "../data/inventory.csv")
-        with open(path,'r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            for row in csv_reader:
-                new_row = Interface(**dict(row))
-                # store_inventory.append(new_row)
-                store_inventory.append({'id': new_row.id, 'title': new_row.title, 'rating': new_row.rating, 'copies_available': new_row.copies_available})
+        with open('data/inventory.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                store_inventory.append({'id': row['id'], 'title': row['title'], 'rating': row['rating'], 'copies_available': int(row['copies_available'])})
+
         return store_inventory
     
+    # read and extract all the information from the customers cvs file
+    @classmethod
+    def read_customers(cls):
+        store_customers = []
+        with open('data/customers.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                store_customers.append({'id': row['id'], 'first_name': row['first_name'],'last_name': row['last_name'],'current_video_rentals': row['current_video_rentals']})
+
+        return store_customers
+    
+    # create a function that prints out a list of all videos in the inventory
     def view_inventory(self):
-        # print(self.inventory)
-        # print(Interface.reading(self))
         for videos in self.inventory:
-            return videos
+            print(videos)
+
+    # create a function to prints out all the videos that have been checked out by the customer 
+    def view_customer_videos(self, id):
+        return_message = ''
+        for customer in self.customers:
+            if str(id) == customer['id']:
+                customer_videos = customer['current_video_rentals'].replace('/', ', ') 
+                return_message = customer_videos
+                break
+            else:
+                return_message = "You are not a registered customer. Please, create an account"
+        print(return_message)
+
+    # write the extracted inventory info back to the cvs file
+    def write_inventory(self):
+        with open('data/inventory.csv', 'w') as csvfile:
+            video_csv = csv.writer(csvfile, delimiter=',')
+            video_csv.writerow(['id','title','rating','copies_available'])
+            for video in self.inventory:
+                video_csv.writerow([video['id'], video['title'], video['rating'], video['copies_available']])
+    
+    # write the extracted customers info back to the cvs file
+    def write_customers(self):
+        with open('data/customers.csv', 'w') as csvfile:
+            customer_csv = csv.writer(csvfile, delimiter=',')
+            customer_csv.writerow(['id','first_name','last_name','current_video_rentals'])
+            for customer in self.customers:
+                customer_csv.writerow([customer['id'], customer['first_name'], customer['last_name'], customer['current_video_rentals']])
+            
+# create a Store class function
+class Store(Interface):
+
+    def __init__(self,id,title,rating,copies_available):
+        super().__init__(id,title,rating,copies_available)
+        self.id = id
+        self.title = title
+        self.rating = rating
+        self.copies_available = copies_available
+        self.customers = Interface.read_customers()
+        self.inventory = Interface.read_inventory()
+
+# create a function that check customer's data to see if the customer has checked out 3 videos or less
+    # the fuction should also check if the video the customer is requesting to check is available in the inventory
+    def check_cust_video(self, id, title):
+        # create a key/value variable that counts the amount a each video available
+        videos_count = {}
+        # create a variable that count number of videos that has been checked out by the customer
+        customer_count = {}
+
+        self.message = ""
+
+        # check the inventory and make sure the number of the video in-stock is greater than 0,
+        # and that the customer has checked out less than 3 video
+            # if this is true, checkout the video as requested and return a message
+        # else, return a message that the customer cannot checkout the video
+        for videos in self.inventory:
+            videos_count[videos['title']] = int(videos['copies_available'])
+            videos['copies_available'] = int(videos['copies_available'])
+            videos['id'] = int(videos['id'])
+            for customer in self.customers:
+                customer['id'] = int(customer['id'])
+                customer_videos = customer['current_video_rentals'].split('/')
+                if customer_videos == ['']:
+                    customer_videos = []
+                    customer_count[customer['id']] = 0
+                else:
+                    customer_count[customer['id']] = int(len(customer_videos))
+        for each_customer in customer_count:
+            for each_video in videos_count:
+                for customer in self.customers:
+                    if int(id) != int(customer['id']):
+                        self.message = "You are not a registered customer!. Please, enter '5' to register for an account."
+                    elif videos_count[title] < 1:
+                        self.message = f"Sorry, we are out of {title}. Please, enter a different title."
+                        break
+                    elif customer_count[id] >= 3:
+                        self.message = "Sorry, you have reached your maximum checkout limit. Please, return some of your  videos to checkout more."
+                        break
+                    elif customer['id'] == int(id) and customer_count[id] < 3:
+                        self.message = "Thanks for renting with us! Come back soon."
+                        break
+        
+
+        return self.message
+
+    # create a function that check the message that was returned in the previous function and modify the inventory if needed
+    def rent_video(self, id, title):
+
+        # make sure the message that was returned in the previous function allows the customer to checkout the video
+            # then change the number of that video in-stock to reflect the video that was checked out
+        # if not, return to homescreen
+        print(Store.check_cust_video(self, id, title))
+        for videos in self.inventory:
+            for customer in self.customers:
+                if Store.check_cust_video(self, id, title) == "Thanks for renting with us! Come back soon.":
+                    if videos['title'] == title and customer['id'] == id:
+                        if customer['current_video_rentals'] == '':
+                            customer['current_video_rentals'] = title
+                            videos['copies_available'] = videos['copies_available'] - 1
+                        else:
+                            customer['current_video_rentals'] = customer['current_video_rentals'] + '/' + title
+                            videos['copies_available'] = videos['copies_available'] - 1
+                else:
+                    Interface.home_screen(self)
+        
+        Interface.write_inventory(self)
+        Interface.write_customers(self)
+    
+    # create a function that check if a customer's information is on file and if the videos the customer is trying to return is listed in the inventory.
+    def customer_return(self, id, title):
+        
+        return_message = "You are not a registered customer or video title is incorrect!. Please, enter '5' to register for an account or enter a correct video title."
+        # if the customer is registered and the video is in inventory, return a message that the return was succesful, and modify the inventory and customer record
+        # else return that the customer cannot return the video
+        for videos in self.inventory:
+            for customer in self.customers:
+                
+                if videos['title'] == title and customer['id'] == str(id):
+                    if customer['current_video_rentals'] == title:
+                        customer['current_video_rentals'] = ''
+                        videos['copies_available'] = videos['copies_available'] + 1
+                        return_message = "Thanks for turning in your video! Come back soon."
+                        break
+                    else:
+                        videos_arr = customer['current_video_rentals'].split('/')
+                        videos_arr.remove(title)
+                        customer['current_video_rentals'] = '/'.join(videos_arr)
+                        videos['copies_available'] = videos['copies_available'] + 1
+                        return_message = "Thanks for turning in your video! Come back soon."    
+
+        Interface.write_inventory(self)
+        Interface.write_customers(self)
+        return return_message
+    
+    # create a function that prints out the return message
+        # this is necessary to be able to connect the function to the spec file. The spec file only reads fuction a return statement
+    def return_video(self, id, title):
+        return_message = Store.customer_return(self, id, title)
+        print(return_message)
+        if return_message != "Thanks for turning in your video! Come back soon.":
+            Interface.home_screen(self)
+        
+
+# create a customers class function
+class Customers(Interface):
+
+    def __init__(self):
+        super().__init__()
+        self.customers = Interface.read_customers()
+    
+    # create a fuction that create accounts for new customers
+    def create_acct(self, new_acct):
+        ids = []
+        new_id = new_acct['id']
+        self.output = ''
+        for each_acct in self.customers:
+            ids.append(int(each_acct['id']))
+
+        # check to make sure that the id chosen by the customer is not already taken
+            # if it is, tell the customer to choose a different id
+        # else, create the customer's account, and return a succes message
+        for i, id in enumerate(ids):
+            if new_id == ids[i]:
+                self.output = "The id you entered is already taken. Please, try a different id."
+                break
+            elif new_id != ids[i]:
+                self.output = "Thanks! Your account has been successfully created."   
+            else:
+                continue
+
+        return self.output
+
+    def add_acct(self, new_acct):
+        
+        # print the message returned from the previous function
+        # if the previous function returns a success message, save the customer's info to the csv file
+        print(Customers.create_acct(self, new_acct))
+        if Customers.create_acct(self, new_acct) == "Thanks! Your account has been successfully created.":
+            self.customers.append(new_acct)
+            Interface.write_customers(self)
+            
+        
+          
+        
+
+        
--- test_spec.py
+++ test_spec.py
@@ -1,19 +1,71 @@
 import unittest
-from optimal_change import my_optimal_change
+from classes.interface import Interface
+from classes.interface import Store
+from classes.interface import Customers
 
 
 class AssesmentTestCase(unittest.TestCase):
-    """Tests for optimal_change.py"""
+    """Tests for interface.py"""
+    """Most of the test cases will not pass after the first trial, because csv files would have been modified"""
 
-    def test_view_inventory(self):
-        output_1 = "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
-        self.assertEqual(my_optimal_change(62.13, 100), output_1)
+    def test_view_video_inventory(self):
+        '''Should print a list of the inventory'''
+        output_1 = [{'id': '1', 'title': 'Guardians of the Galaxy', 'rating': 'PG-13', 'copies_available': 4}, {'id': '2', 'title': 'Prometheus', 'rating': 'R', 'copies_available': -14}, {'id': '3', 'title': 'Split', 'rating': 'PG-13', 'copies_available': 2}, {'id': '4', 'title': 'Sing', 'rating': 'PG', 'copies_available': 10}, {'id': '5', 'title': 'La La Land', 'rating': 'PG-13', 'copies_available': 0}, {'id': '6', 'title': 'WALL-E', 'rating': 'G', 'copies_available': 3}, {'id': '7', 'title': 'The Prestige', 'rating': 'PG-13', 'copies_available': 1}, {'id': '8', 'title': 'The Dark Knight', 'rating': 'PG-13', 'copies_available': 5}, {'id': '9', 'title': 'Inception', 'rating': 'PG-13', 'copies_available': 2}, {'id': '10', 'title': 'Interstellar', 'rating': 'PG-13', 'copies_available': -5}]
+        self.assertEqual(Interface.read_inventory(), output_1)
 
-    def test_second_case(self):
-        '''Check for plural and commas'''
-        output_2 = "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
-        self.assertEqual(my_optimal_change(31.51, 50), output_2)
+    def test_view_customers_list(self):
+        '''Should print a list of customers'''
+        output_2 = [{'id': '1', 'first_name': 'Jon', 'last_name': 'Young', 'current_video_rentals': 'Guardians Of The Galaxy/La La Land'}, {'id': '2', 'first_name': 'Tom', 'last_name': 'Prete', 'current_video_rentals': 'Prometheus/Split'}, {'id': '3', 'first_name': 'Rod', 'last_name': 'Levy', 'current_video_rentals': 'The Dark Knight/Guardians Of The Galaxy/Split'}, {'id': '4', 'first_name': 'Ankur', 'last_name': 'Shah', 'current_video_rentals': 'The Prestige'}, {'id': '5', 'first_name': 'Chris', 'last_name': 'Howell', 'current_video_rentals': 'Sing'}, {'id': '6', 'first_name': 'Matt', 'last_name': 'Jake', 'current_video_rentals': 'Guardians of the Galaxy'}, {'id': '7', 'first_name': 'Musa', 'last_name': 'Sho', 'current_video_rentals': ''}, {'id': '8', 'first_name': 'Nate', 'last_name': 'Juan', 'current_video_rentals': 'Split'}]
+        self.assertEqual(Interface.read_customers(), output_2)
+
+    def test_create_new_acct(self):
+        '''Should create an acct for a new customer'''
+        self.customers = Interface.read_customers()
+        new_acct = {'id': 14, 'first_name': 'Jamal', 'last_name': 'Lawrence', 'current_video_rentals': ''}
+        output_3 = "Thanks! Your account has been successfully created."
+        self.assertEqual(Customers.create_acct(self, new_acct), output_3)
+    
+    def test_rent_video(self):
+        '''Should rent a video'''
+        self.customers = Interface.read_customers()
+        self.inventory = Interface.read_inventory()
+        output_4 = "Thanks for renting with us! Come back soon."
+        self.assertEqual(Store.check_cust_video(self, 4, 'Sing'), output_4)
+    
+    def test_return_video(self):
+        '''Should return a video'''
+        self.customers = Interface.read_customers()
+        self.inventory = Interface.read_inventory()
+        output_5 = "Thanks for turning in your video! Come back soon."
+        self.assertEqual(Store.customer_return(self, 5, "The Dark Knight"), output_5)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+'''
+# This can be pasted in csv files to retest
+
+id,title,rating,copies_available
+1,Guardians of the Galaxy,PG-13,4
+2,Prometheus,R,-14
+3,Split,PG-13,2
+4,Sing,PG,10
+5,La La Land,PG-13,0
+6,WALL-E,G,3
+7,The Prestige,PG-13,1
+8,The Dark Knight,PG-13,4
+9,Inception,PG-13,2
+10,Interstellar,PG-13,-5
+
+id,first_name,last_name,current_video_rentals
+1,Jon,Young,Guardians Of The Galaxy/La La Land
+2,Tom,Prete,Prometheus/Split
+3,Rod,Levy,The Dark Knight/Guardians Of The Galaxy/Split
+4,Ankur,Shah,The Prestige
+5,Chris,Howell,Sing/The Dark Knight
+6,Matt,Jake,Guardians of the Galaxy
+7,Musa,Sho,
+8,Nate,Juan,Split
+
+'''
